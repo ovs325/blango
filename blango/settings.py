073995abe1c1538edcd98ffa9f3cf1e773ae60fe
Сухоруков Геннадьевич <5025554@gmail.com>
2022-04-01 09:47:56 +0000

    Finish logging

--- blango/settings.py
+++ blango/settings.py
@@ -139,3 +139,26 @@ SESSION_COOKIE_SAMESITE = 'None'
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {module} {process:d} {thread:d} msg: {message}",
+            "style": "{",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+            "formatter": "verbose",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
+    },
+}
+
