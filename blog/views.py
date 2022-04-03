d3b880dc26f1d26f842a2c0f7cad85b9970c0445
Сухоруков Геннадьевич <5025554@gmail.com>
2022-04-03 07:07:01 +0000

    Finish caching

--- blog/views.py
+++ blog/views.py
@@ -8,9 +8,7 @@ logger = logging.getLogger(__name__)
 
 def index(request):
     posts = Post.objects.filter(published_at__lte=timezone.now())
-    
     logger.debug("Got %d posts", len(posts))
-
     return render(request, "blog/index.html", {"posts": posts})
 
 def post_detail(request, slug):
