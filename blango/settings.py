bdc87a001bdac7c0fa129833f000a447a6de0ff7
Сухоруков Геннадьевич <5025554@gmail.com>
2022-04-04 17:58:07 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -25,8 +25,11 @@ class Dev(Configuration):
     AUTH_USER_MODEL = "blango_auth.User"
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
     ACCOUNT_ACTIVATION_DAYS = 7
-
-    # Application definition
+    SITE_ID = 1
+    ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_USERNAME_REQUIRED = False
+    ACCOUNT_AUTHENTICATION_METHOD = "email"
 
     INSTALLED_APPS = [
         'django.contrib.admin',
@@ -34,12 +37,17 @@ class Dev(Configuration):
         'django.contrib.contenttypes',
         'django.contrib.sessions',
         'django.contrib.messages',
+        'django.contrib.sites',
         'django.contrib.staticfiles',
         'crispy_forms',
         'crispy_bootstrap5',
         'blango_auth',
         'blog',
         "debug_toolbar",
+        "allauth", 
+        "allauth.account", 
+        "allauth.socialaccount", 
+        "allauth.socialaccount.providers.google"
     ]
     
     MIDDLEWARE = [
--- blango/urls.py
+++ blango/urls.py
@@ -19,6 +19,7 @@ urlpatterns = [
         ),
     path("accounts/", include("django_registration.backends.activation.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path("", blog.views.index),
 ]
