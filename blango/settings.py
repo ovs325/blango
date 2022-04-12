f09d6001fd8b1a95e8a3ea86d0852b2616d5e915
Сухоруков Геннадьевич <5025554@gmail.com>
2022-04-12 15:28:56 +0000

    Finish authentication

--- blango/settings.py
+++ blango/settings.py
@@ -49,6 +49,7 @@ class Dev(Configuration):
         "allauth.socialaccount", 
         "allauth.socialaccount.providers.google",
         "rest_framework",
+        "rest_framework.authtoken",
     ]
     
     MIDDLEWARE = [
@@ -184,3 +185,10 @@ PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ]
+}
