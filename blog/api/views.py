cd2f5adf6df4e2b47074a6d0d8a46203b4ca71de
Сухоруков Геннадьевич <5025554@gmail.com>
2022-04-12 19:38:53 +0000

    Finish related fields

--- blog/api/views.py
+++ blog/api/views.py
@@ -4,6 +4,9 @@ from blog.api.serializers import PostSerializer
 from blog.models import Post
 from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
 
+from blango_auth.models import User
+from blog.api.serializers import PostSerializer, UserSerializer
+
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
@@ -14,3 +17,9 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    lookup_field = "email"
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
